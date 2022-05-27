45038f2fe9368afc1fffaa5c02b02d2c0b15249a
Dany Giguere <danygiguere23@gmail.com>
2022-05-27 18:24:26 -0400

    removed middlewares because they are not working anymore with ktor 2

--- src/main/kotlin/com/example/routes/demoRoutes.kt
+++ src/main/kotlin/com/example/routes/demoRoutes.kt
@@ -1,9 +1,12 @@
 package com.example.routes
 
+import at.favre.lib.crypto.bcrypt.BCrypt
+import com.example.plugins.AuthorizationException
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.Identity.encode
 
 fun Route.demoRoutes() {
 
@@ -20,8 +23,14 @@ fun Route.demoRoutes() {
     }
 
     get("/demo/signed-url") {
-        // you must add the url param ?signature=1234 for this route to respond successfully
-//            hasSignedUrlMiddleware(context as ApplicationCall)
+        // you must add the url param ?signature=$2a$12$DvbEVyvGzearyPn7.c0QQO1edR2R7zyo68Aso/qIntA3KI1mMCe82 for this route to respond successfully
+        // $2a$12$DvbEVyvGzearyPn7.c0QQO1edR2R7zyo68Aso/qIntA3KI1mMCe82 is the hashed route /demo/signed-url
+        // I need to replace the hash with an encryption because the hash is not url friendly (and ugly)
+        val signature = context.request.queryParameters["signature"]
+        val result: BCrypt.Result = BCrypt.verifyer().verify("/demo/signed-url".toCharArray(), signature)
+        if(!result.verified) {
+            throw AuthorizationException("Sorry you are not authorized")
+        }
         call.respondText("This is a text response from demoRoutes@/demo/signed-url")
     }
 
